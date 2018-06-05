d3282753bc70e54728736f23b6c670fee107189a
Honza Hommer <honza@hommer.cz>
2018-06-05 16:02:19 +0200

    Update PM2 ecosystem for another vhost

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,19 +8,19 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'app',
-      host: 'johnson.hommer.cz',
+      user: 'root',
+      host: 'jobsdev-2018.hommer.cz',
       ref: 'origin/master',
       repo: 'git@github.com:honzahommer/jobsdev-2018.git',
-      path: '/vol/app/jobsdev/production',
+      path: '/var/app/jobsdev-2018/production',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     development: {
-      user: 'app',
-      host: 'johnson.hommer.cz',
+      user: 'root',
+      host: 'jobsdev-2018.hommer.cz',
       ref: 'origin/master',
       repo: 'git@github.com:honzahommer/jobsdev-2018.git',
-      path: '/vol/app/jobsdev/development',
+      path: '/var/app/jobsdev-2018/development',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env development',
       env: {
         NODE_ENV: 'development'
