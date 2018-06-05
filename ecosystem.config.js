8be207e6ca454c81aea6decc587f130e5d76e830
Honza Hommer <honza@hommer.cz>
2018-06-05 15:30:13 +0200

    Base app

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [{
+    name: 'jobsdev',
+    script: 'bin/www',
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+  deploy: {
+    production: {
+      user: 'app',
+      host: 'johnson.hommer.cz',
+      ref: 'origin/master',
+      repo: 'git@github.com:honzahommer/jobsdev-2018.git',
+      path: '/var/app/jobsdev/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    development: {
+      user: 'app',
+      host: 'johnson.hommer.cz',
+      ref: 'origin/master',
+      repo: 'git@github.com:honzahommer/jobsdev-2018.git',
+      path: '/var/app/jobsdev/development',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env development',
+      env: {
+        NODE_ENV: 'development'
+      }
+    }
+  }
+};
